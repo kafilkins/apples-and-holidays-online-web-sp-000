2cc08be7fec3381d6b9e1fc0802a3ea43ca3648c
Keith Filkins <kafilkins@me.com>
2020-12-06 19:16:32 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -27,7 +27,7 @@ def add_supply_to_winter_holidays(holiday_hash, supply)
   # Christmas AND the New Year's arrays
   holiday_hash[:winter].each do |holiday, decorations|
     decorations << supply
-    binding.pry 
+    binding.pry
   end
 end
 
