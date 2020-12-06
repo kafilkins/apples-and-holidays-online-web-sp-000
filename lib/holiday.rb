c06de54fa0f3428c2134d64c2358d1120e9fb444
Keith Filkins <kafilkins@me.com>
2020-12-06 19:39:38 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -64,7 +64,7 @@ def all_supplies_in_holidays(holiday_hash)
     puts "#{season.capitalize}:"
     holiday.each do |holiday, supplies|
     puts "  #{holiday.to_s.split("_").map{|x| x.capitalize}.join(" ")}: #{supplies.join(", ")}"
-  end
+    end
   end
 end
 
@@ -73,3 +73,4 @@ def all_holidays_with_bbq(holiday_hash)
   # include the string "BBQ"
 
 end
+  
